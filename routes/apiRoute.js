4ab0191c25c6b76bc2aaeef4b55a54ea661725a9
Darbaz Ali <darbaz.a.h@gmail.com>
2020-10-01 23:59:06 +0300

    validate posted url

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,18 +1,29 @@
 import { generate } from 'shortid'
+import { isUri } from 'valid-url'
 
 const apiURL = (app, URL) => {
     app.route('/api/url')
         .post((req, res) => {
             const { url } = req.body
-            const newURL = new URL({
-                short_url: generate(),
-                original_url: url,
-            })
+            if (!isUri(url)) {
+                res.send('This is not avalid url')
+            } else {
+                const newURL = new URL({
+                    short_url: generate(),
+                    original_url: url,
+                })
 
-            newURL
-                .save()
-                .then((data) => res.json(data))
-                .catch((err) => res.send(err))
+                newURL
+                    .save()
+                    .then((data) => {
+                        const host = req.headers.host
+                        res.render('index', {
+                            original_url: data.original_url,
+                            short_url: host + '/' + data.short_url,
+                        })
+                    })
+                    .catch((err) => res.send(err))
+            }
         })
         .get((req, res) => {
             res.send('urls')
